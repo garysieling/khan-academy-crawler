5edc6fe77c882c7ef6ab3bf6bf909b6de9c64d0d
Gary Sieling <gary@garysieling.com>
2018-10-29 21:57:44 -0400

    classification

---
+++ classification1.sh
@@ -0,0 +1,33 @@
+DATADIR=`pwd`
+MODELDIR=`pwd`
+
+~/projects/StarSpace/starspace train \
+  -trainFile "${DATADIR}"/train.txt \
+  -model "${MODELDIR}"/model \
+  -initRandSd 0.01 \
+  -adagrad false \
+  -ngrams 1 \
+  -lr 0.01 \
+  -epoch 5 \
+  -thread 20 \
+  -dim 10 \
+  -negSearchLimit 5 \
+  -maxNegSamples 3 \
+  -trainMode 0 \
+  -label "__label__" \
+  -similarity "dot" \
+  -verbose true
+
+echo "Start to evaluate trained model:"
+
+~/projects/StarSpace/starspace test \
+  -model "${MODELDIR}"/model \
+  -testFile "${DATADIR}"/test.txt \
+  -ngrams 1 \
+  -dim 10 \
+  -label "__label__" \
+  -thread 10 \
+  -similarity "dot" \
+  -trainMode 0 \
+  -verbose true
+
